a7c5c70e116ea6f863abf2d520d8a632bddf19d9
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-08-21 12:48:55 -0500

    Creating visual requirements

--- Hamburguesas/ViewController.swift
+++ Hamburguesas/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelHamburguesa: UILabel!
+    @IBOutlet weak var labelPais: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonHamburguesa() {
     }
 
 
